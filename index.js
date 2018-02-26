583437f59caa72df606b840f2d6b157ce9ad7517
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-27 09:24:47 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.login(process.env.BOT_TOKEN
+client.on("message", message => {
+  if (message.author.id === "415704844221808640") message.reply("**Busta!** Straight **Busta!**");
+});
+
+client.on("message", message => {
+  if (message.author.id === "415330227451985920") message.reply("Shiet you look **jacked** up!");
+});
+
+client.on("message", message => {
+  if (message.author.id === "416418286184431636") message.reply("Watch the road, **motherfucker**, the road!");
+});
+
+client.on("message", message => {
+  if (message.author.id === "417500499424706560") message.reply("Oh man, we gonna **die!**");
+});
+
+client.login(process.env.BOT_TOKEN);
