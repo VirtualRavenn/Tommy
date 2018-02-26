3d1ca999faa3fac4c0f389e49aabdc1c8016d338
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-27 09:22:47 +1100

    Update index.js

--- index.js
+++ index.js
@@ -5,10 +5,4 @@ client.on("message", message => {
   if (message.author.id === "") message.reply("This is the last dance for Lance Vance!");
 });
 
-client.on('message', msg => {
-  if (msg.content.toLowerCase()=== "ha gae!") {
-    msg.reply("Imma bash your head in **fool.**");
-  }
-});
-
 client.login(process.env.BOT_TOKEN
