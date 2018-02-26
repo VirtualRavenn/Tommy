93cef0e0106d008e6f85dae034482398cb594b35
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-27 09:23:53 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-client.on("message", message => {
-  if (message.author.id === "") message.reply("This is the last dance for Lance Vance!");
-});
-
 client.login(process.env.BOT_TOKEN
